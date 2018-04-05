659abb299a9dc22eda22e88630a23f58d295de0c
metanas <com.com404@gmail.com>
2018-04-05 19:09:01 +0100

    create Pdf Class

--- WindowsFormsApp7/Form1.cs
+++ WindowsFormsApp7/Form1.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using iText.IO.Font;
+using iText.IO.Image;
+using iText.Kernel.Colors;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
+using iText.Layout;
+using iText.Layout;
+using iText.Layout.Borders;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +29,24 @@ namespace WindowsFormsApp7
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PdfCreator pdf = new PdfCreator();
+            pdf.CreateCommandPdf("aaaaaa.pdf");
+
+            //cell.SetNextRenderer(new WatermarkedCellRenderer(cell, "Bruno"));
+            //PdfFont font = PdfFontFactory.CreateFont(FontProgramFactory.CreateFont(FontConstants.HELVETICA_BOLD));
+            //Paragraph para = new Paragraph("Test").SetFont(font);
+            //para.SetFixedLeading(0);
+            //para.SetMultipliedLeading(1);
+            //table.SetHorizontalAlignment(iText.Layout.Properties.HorizontalAlignment.RIGHT);
+            //cell.SetHeight(20);
+            //cell.SetWidth(100);
+            //cell.SetVerticalAlignment(VerticalAlignment.MIDDLE);
+            //cell.Add(para);
+            //table.AddCell(cell);
+
         }
     }
 }
---
+++ WindowsFormsApp7/PdfCreator.cs
@@ -0,0 +1,25 @@
+﻿using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp7
+{
+    class PdfCreator
+    {
+        string img = @"C:\Users\Dell\Desktop\LogoNew.png";
+
+        public bool CreateCommandPdf(string fileName) { 
+            PdfDocument pdf = new PdfDocument(new PdfWriter(fileName));
+            Document doc = new Document(pdf);
+
+            doc.Add(new Cell().Add(new iText.Layout.Element.Image(ImageDataFactory.Create(img)).SetAutoScaleWidth(true)));
+            return true;
+        }
+    }
+}
